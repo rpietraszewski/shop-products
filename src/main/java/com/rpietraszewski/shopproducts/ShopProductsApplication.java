882380d70a669bc4d67b4c661d1486ee3777ee7b
Robert Pietraszewski <80597@student.pb.edu.pl>
2024-04-23 23:59:36 +0200

    Project Initialization

---
+++ src/main/java/com/rpietraszewski/shopproducts/ShopProductsApplication.java
@@ -0,0 +1,13 @@
+package com.rpietraszewski.shopproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpietraszewski/shopproducts/ShopProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpietraszewski.shopproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
